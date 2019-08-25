73943a9947b243fae9d75d8eed6605853bbd48e1
Florian Ewald <florian.ewald@sinnerschrader.com>
2019-08-25 21:13:45 +0200

    update

--- integration/src/index.ts
+++ integration/src/index.ts
@@ -1,6 +1,8 @@
 import { Doctor } from "@addtocart/doctor";
 import { Patient } from "@addtocart/patient";
 import { Scheduler } from "@addtocart/scheduler";
+import { MyElement } from "@addtocart/oo-wc-helloworld";
+
 
 console.log(new Doctor());
 console.log(new Scheduler());
