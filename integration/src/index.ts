48495692f97d83edac0bc04d4f92556c694bec3e
Florian Ewald <florian.ewald@sinnerschrader.com>
2019-08-25 18:38:12 +0200

    add yarn workspaces

---
+++ integration/src/index.ts
@@ -0,0 +1,7 @@
+import { Doctor } from "@addtocart/doctor";
+import { Patient } from "@addtocart/patient";
+import { Scheduler } from "@addtocart/scheduler";
+
+console.log(new Doctor());
+console.log(new Scheduler());
+console.log(new Patient());
